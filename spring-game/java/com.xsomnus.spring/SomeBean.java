d0af62a506ddbb1e1d32653525faf42253fa0a92
xsomnus <xwyttkx@qq.com>
2018-12-31 15:30:48 +0800

    initsss

---
+++ spring-game/java/com.xsomnus.spring/SomeBean.java
@@ -0,0 +1,18 @@
+package com.xsomnus.spring;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @author xsomnus_xiawenye
+ * @since 2018-12-31 15:27
+ **/
+
+public class SomeBean implements InitializingBean {
+
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
